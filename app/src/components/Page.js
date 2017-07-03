fdae62faa6fef6c355207c23e3f67f33571302e1
xuyijun <xuyijun@sansi.com>
2017-07-03 16:36:57 +0800

    Add Page

---
+++ app/src/components/Page.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import '../../public/styles/rc-pagination.css';
+import Pagination from 'rc-pagination';
+//分页器
+export default class Page extends Component {
+    constructor(props){
+        super(props)
+        this.paginationOnChange = this.paginationOnChange.bind(this);
+    }
+
+    paginationOnChange(page){
+        this.props.onChange && this.props.onChange(page);
+    }
+
+    render() {
+        let props = this.props;
+        return (
+            <Pagination  {...props} onChange={this.paginationOnChange}/>
+        )
+    }
+}
