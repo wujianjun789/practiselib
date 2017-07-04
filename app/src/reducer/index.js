37fa08ecf5487a163ac2f4755b8c8901abd4fbb0
吴勇 <wuyong@sansi.com>
2017-07-04 15:18:00 +0800

    fixed #10935

--- app/src/reducer/index.js
+++ app/src/reducer/index.js
@@ -5,11 +5,13 @@ import test from './test';
 import course from './course';
 
 import login from '../login/reducer/index'
+import assetManage from '../assetManage/reducer/index'
 const reducer = combineReducers({
     routing,
     course,
     test,
 
-    login
+    login,
+    assetManage
 });
 export default reducer;
