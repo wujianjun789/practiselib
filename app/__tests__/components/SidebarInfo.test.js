1a3424e7f67755546a1b8925d9973502f9c2b241
吴勇 <wuyong@sansi.com>
2017-07-06 10:54:05 +0800

    fixed #10965

---
+++ app/__tests__/components/SidebarInfo.test.js
@@ -0,0 +1,32 @@
+/**
+ * Created by a on 2017/7/6.
+ */
+import React from 'react';
+import renderer from 'react-test-renderer'
+import {shallow} from 'enzyme';
+// import SideBarInfo from '../../src/components/SideBarInfo'
+// test('SidebarInfo renders', ()=>{
+//
+//     const component = renderer.create(
+//         <SideBarInfo />
+//     )
+//
+//     let sideBarInfo = component.toJSON();
+//     expect(sideBarInfo).toMatchSnapshot();
+//
+//     sideBarInfo.find('collapse-container').props.onClick();
+//
+//     sideBarInfo = component.toJSON();
+//     expect(sideBarInfo).toMatchSnapshot();
+// })
+
+// test('SidebarInfo div click', ()=>{
+//     const component = shallow(<SideBarInfo />)
+//
+//     component.find('.collapse-container').simulate('click');
+//     expect(component.is('.sidebar-collapse')).toEqual(true);
+// })
+
+test(()=>{
+
+})
