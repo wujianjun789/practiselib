7b86ac08ae9bed7831b1e1b32172bf9f31532740
xuyijun <xuyijun@sansi.com>
2017-07-12 09:48:11 +0800

    overlayer单元测试

--- app/__tests__/overlayer/components/overlayer.test.js
+++ app/__tests__/overlayer/components/overlayer.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
+import renderer from 'react-test-renderer';
 import {Overlayer} from '../../../src/overlayer/containers/overlayer';
+import AlterPwPopup from '../../../src/userCenter/containers/AlterPwPopup';
 
 describe('<Overlayer />', () => {
-    it('', () => {
-
+    let data1 = {
+        isShowDialog:true,
+        page: <AlterPwPopup className='alter-pw-popup'/>
+    };
+    let data2 = {
+        isShowDialog:false,
+        page:null
+    };
+    
+    it('render show', () => {
+        const overlayer = shallow(<Overlayer overlayer={data1}/>)
+        const show = overlayer.find('.overlayer-show');
+        expect(show.length).toBe(1);
     })
+
+    it('render hidden', () => {
+        const overlayer = shallow(<Overlayer overlayer={data2}/>)
+        const show = overlayer.find('.overlayer-show');
+        expect(show.length).toBe(0);
+    })
+
+    it('snapshot show', () => {
+        const cmp = renderer.create(<Overlayer overlayer={data1}/>);
+        const tree = cmp.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('snapshot hidden', () => {
+        const cmp = renderer.create(<Overlayer overlayer={data2}/>);
+        const tree = cmp.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 })
--- app/__tests__/overlayer/containers/overlayer.test.js
+++ app/__tests__/overlayer/containers/overlayer.test.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import configureStore from '../../../src/store/configureStore';
+import Overlayer from '../../../src/overlayer/containers/overlayer';
+import {initialState as state} from '../../../src/App/reducer';
+
 describe('<Overlayer /> HOC', () => {
-    it('', () => {
-        
+    const store = configureStore();
+    it('render normal', () => {
+        const root = mount(<Provider store = {store}>
+            <Overlayer />
+        </Provider>);
+
+        const overlayer = root.find('Overlayer');
+        expect(overlayer.length).toBe(1);
+        expect(overlayer.prop('isShowDialog')).toBe(state.isShowDialog);
+        expect(overlayer.prop('page')).toBe(state.page);
     })
 })
