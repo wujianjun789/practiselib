6036eefb630ade964fd2ceef2e192915628aa258
Azrael <wuyanbin@sansi.com>
2017-07-27 14:33:36 +0800

    更新ExitPopup组件，更新测试代码

--- app/src/components/ExitPopup.js
+++ app/src/components/ExitPopup.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import PanelFooter from './PanelFooter';
 
-const ExitPopup = ({cancel, confirm}) => {
+const ExitPopup = ({tips='', iconClass='', cancel, confirm}) => {
     return <div className="usr-exit">
         <div className='tips'>
-            <div><span className="icon icon-popup-exit"></span></div>
-            是否退出？
+            <div><span className={`icon ${iconClass}`}></span></div>
+            {tips}
         </div>
         <div className="btn-toolbar">
             <div className='btn-group'><button className="btn btn-default" onClick={cancel}>Cancel</button></div>
             <div className='btn-group'><button className="btn btn-primary" onClick={confirm}>Confirm</button></div>
         </div>
     </div>
+}
+
+ExitPopup.prototype = {
+    tips: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node
+    ]).isRequired,
+    iconClass: PropTypes.string.isRequired,
+    cancel: PropTypes.func.isRequired,
+    confirm: PropTypes.func.isRequired
 }
 
 export default ExitPopup;
