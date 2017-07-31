4c07d3a77eacc224174c326ae6a0b62caf4525fb
吴勇 <wuyong@sansi.com>
2017-07-31 14:33:39 +0800

    fixed #11598

--- app/src/domainMapPreview/container/MapPreview.js
+++ app/src/domainMapPreview/container/MapPreview.js
@@ -3,8 +3,12 @@
  */
 import React,{Component} from 'react'
 
+import '../../../public/styles/domain-mapPreview.less';
 import Content from '../../components/Content'
 import MapView from '../../components/MapView'
+
+import SearchText from '../../components/SearchText'
+
 export default class MapPreview extends Component{
     constructor(props){
         super(props)
@@ -22,14 +26,31 @@ export default class MapPreview extends Component{
                     id: 1,
                     name: '上海市'
                 }
-            }
+            },
+            search:{placeholder:'  输入域名称搜索', value:'', datalist:[{id:1, value:"domain1"},{id:2, value:"domain2"}]}
         }
+
+        this.onChange = this.onChange.bind(this);
+        this.searchSubmit = this.searchSubmit.bind(this);
+    }
+
+    onChange(value){
+        const { search } = this.state;
+
+        let newValue = Object.assign({}, search, {value:value});
+        this.setState({search:newValue});
+    }
+    
+    searchSubmit(){
+
     }
 
     render(){
-        const {selectDomain} = this.state;
-        return <Content>
+        const {selectDomain, search} = this.state;
+        return <Content className="map-preview">
             <MapView mapData={selectDomain}/>
+            <SearchText IsTip={true} datalist={search.datalist} placeholder={search.placeholder} value={search.value}
+                        onChange={this.onChange} submit={this.searchSubmit}/>
             </Content>
     }
 }
