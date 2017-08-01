56fa77af57fce2f933b6cb83f253c3cd067e84f5
吴勇 <wuyong@sansi.com>
2017-08-01 16:33:25 +0800

    fixed #11622

--- app/src/domainManage/container/Topology.js
+++ app/src/domainManage/container/Topology.js
@@ -10,10 +10,10 @@ export default class Topology extends Component{
         this.state = {
             language: getLanguage(),
             domainList:{zh:[
-                {id:1, name:"中国", active:true, children:[
+                {id:1, name:"中国中国中国", active:true, children:[
                     {id:11, name:"上海", active:true, children:[
                         {id:112, name:"徐汇"},
-                        {id:113, name:"浦东新区新区"},
+                        {id:113, name:"浦东新区新区浦东"},
                         {id:111, name:"闵行区", active:true, children:[
                             {id:1113, name:"颛桥镇"},
                             {id:1111, name:"莘庄镇", active:true, children:[
@@ -29,7 +29,7 @@ export default class Topology extends Component{
                 {id:2, name:"日本"}
             ],
             en:[
-                {id:1, name:"China", active:true, children:[
+                {id:1, name:"ChinaChina", active:true, children:[
                     {id:11, name:"Shanghai", active:true, children:[
                         {id:112, name:"xuhui"},
                         {id:113, name:"pudongxinqu"},
@@ -51,6 +51,7 @@ export default class Topology extends Component{
 
         this.getDatalist = this.getDatalist.bind(this);
         this.renderChild = this.renderChild.bind(this);
+        this.itemClick = this.itemClick.bind(this);
     }
 
     componentWillMount(){
@@ -61,6 +62,10 @@ export default class Topology extends Component{
 
     }
 
+    itemClick(item){
+        console.log(item);
+    }
+
     getDatalist(list, renderList){
         list.map(domain=>{
             domain.active && renderList.push(domain) && domain.children && this.getDatalist(domain.children, renderList);
@@ -69,14 +74,12 @@ export default class Topology extends Component{
     }
 
     renderChild(list, depth){
-        if(depth>2){
-
-        }
-
         return <ul key={depth} className={"topology-"+depth}>
             {
                 list.map(item=>{
-                    return <li key={item.id} className={item.active?'active':''}><div>{item.name}</div></li>
+                    let value = item.name.slice(0, this.state.language=='zh'?6:10);
+                    return <li key={item.id} className={item.active?'active':''} title={item.name}
+                    onClick={()=>this.itemClick(item)}><div>{value}</div></li>
                 })
             }
         </ul>
