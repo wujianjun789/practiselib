d57bd3a7d72302176b8f82ea480c991395910fc5
Azrael <wuyanbin@sansi.com>
2017-08-08 10:30:21 +0800

    修改弹框样式及首页样式及删除密码弹框最外层容器

--- app/src/components/AlterPwPopup.js
+++ app/src/components/AlterPwPopup.js
@@ -35,27 +35,25 @@ export default class AlterPwPopup extends PureComponent {
     render() {
         let {className=''} = this.props;
         let footer = <PanelFooter funcNames={['onCancel','onConfirm']} btnTitles={['取消','确认']} btnClassName={['btn-default', 'btn-primary']} btnDisabled={[false, this.state.repPwState]} onCancel={this.onCancel} onConfirm={this.onConfirm}/>;
-        return <div className={className}>
-            <Panel title='修改密码'  footer={footer} closeBtn={true} closeClick={this.onCancel}>
-                <div className="form-group row">
-                    <label htmlFor="oldPw" className="col-sm-3 control-label">旧密码：</label>
-                    <div className="col-sm-9">
-                        <input type="password" className="form-control" id="oldPw" placeholder="输入旧密码" value={this.state.oldPw} onChange={this.onChange}/>
-                    </div>
+        return <Panel className={className} title='修改密码'  footer={footer} closeBtn={true} closeClick={this.onCancel}>
+            <div className="form-group row">
+                <label htmlFor="oldPw" className="col-sm-3 control-label">旧密码：</label>
+                <div className="col-sm-9">
+                    <input type="password" className="form-control" id="oldPw" placeholder="输入旧密码" value={this.state.oldPw} onChange={this.onChange}/>
                 </div>
-                <div className={`form-group row`}>
-                    <label htmlFor="newPw" className="col-sm-3 control-label">新密码：</label>
-                    <div className="col-sm-9">
-                        <input type="password" className="form-control" id="newPw" placeholder="输入新密码" value={this.state.newPw} onChange={this.onChange}/>
-                    </div>
+            </div>
+            <div className={`form-group row`}>
+                <label htmlFor="newPw" className="col-sm-3 control-label">新密码：</label>
+                <div className="col-sm-9">
+                    <input type="password" className="form-control" id="newPw" placeholder="输入新密码" value={this.state.newPw} onChange={this.onChange}/>
                 </div>
-                <div className={`form-group row ${this.state.repPwState ? 'has-error' : ''}`}>
-                    <label htmlFor="repPw" className="col-sm-3 control-label">重复密码：</label>
-                    <div className="col-sm-9">
-                        <input type="password" className="form-control" id="repPw" placeholder="再次输入新密码" value={this.state.repPw} onChange={this.onChange}/>
-                    </div>
+            </div>
+            <div className={`form-group row ${this.state.repPwState ? 'has-error' : ''}`}>
+                <label htmlFor="repPw" className="col-sm-3 control-label">重复密码：</label>
+                <div className="col-sm-9">
+                    <input type="password" className="form-control" id="repPw" placeholder="再次输入新密码" value={this.state.repPw} onChange={this.onChange}/>
                 </div>
-            </Panel>
-        </div>
+            </div>
+        </Panel>
     }
 }
