6382e7a78dffb60686c5b9144425b3837821c543
吴勇 <wuyong@sansi.com>
2017-08-10 15:04:01 +0800

    fixed #修改大小写

--- app/__tests__/app/container/App.test.js
+++ app/__tests__/app/container/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {mount} from 'enzyme';
 import {Provider} from 'react-redux';
 import configureStore from '../../../src/store/configureStore';
-import App from '../../../src/App/container';
-import {initialState as state} from '../../../src/App/reducer';
+import App from '../../../src/app/container';
+import {initialState as state} from '../../../src/app/reducer';
 
 describe('<app /> HOC', () => {
     const store = configureStore();
--- app/__tests__/app/reducer/app.test.js
+++ app/__tests__/app/reducer/app.test.js
@@ -1,4 +1,4 @@
-import app, {initialState as state} from '../../../src/App/reducer/index';
+import app, {initialState as state} from '../../../src/app/reducer/index';
 
 test('test app reducer', () => {
     expect(app(undefined, {})).toEqual(state);
