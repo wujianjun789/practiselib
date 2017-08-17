9457003280dd7c172df6fc94a80cd8e1009af878
Azrael <wuyanbin@sansi.com>
2017-08-17 11:36:14 +0800

    传感器策略添加分页

--- app/src/controlStrategy/container/SensorStrategy.js
+++ app/src/controlStrategy/container/SensorStrategy.js
@@ -11,6 +11,7 @@ import Table from '../../components/Table';
 import {overlayerHide, overlayerShow} from '../../common/actions/overlayer';
 import SensorStrategyPopup from '../component/SensorStrategyPopup';
 import ConfirmPopup from '../../components/ConfirmPopup';
+import Page from '../../components/Page';
 import Immutable from 'immutable';
 
 export class SensorStrategy extends Component{
@@ -23,6 +24,11 @@ export class SensorStrategy extends Component{
             search: {
                 placeholder: '输入策略名称',
                 value: ''
+            },
+            page: {
+                pageSize: 10,
+                current: 1,
+                total: 1
             }
         }
 
@@ -38,6 +44,7 @@ export class SensorStrategy extends Component{
         this.tableRowEdit = this.tableRowEdit.bind(this);
         this.tableRowDelete = this.tableRowDelete.bind(this);
         this.confirmDelete = this.confirmDelete.bind(this);
+        this.pageChange = this.pageChange.bind(this);
         
     }
 
@@ -70,6 +77,10 @@ export class SensorStrategy extends Component{
 
     }
 
+    pageChange(page) {
+        
+    }
+
     onClick(e) {
         const {id} = e.target;
         const initData = {
@@ -89,7 +100,7 @@ export class SensorStrategy extends Component{
     }
 
     render() {
-        const {data, search} = this.state;
+        const {data, search, page} = this.state;
         return <Content className="sensor-strategy">
             <div className="content-title">
                 <SearchText placeholder={search.placeholder} value={search.value} onChange={this.searchChange} submit={this.searchSubmit}/>
@@ -97,6 +108,7 @@ export class SensorStrategy extends Component{
             </div>
             <div className="content-body">
                 <Table columns={this.columns} keyField='id' data={Immutable.fromJS(data)} isEdit rowEdit={this.tableRowEdit} rowDelete={this.tableRowDelete} rowClick={this.tableRowClick}/>
+                <div className='pagination'><Page className={`${page.total==0?"hidden":''}`} showSizeChanger pageSize={page.pageSize} current={page.current} total={page.total}  onChange={this.pageChange}/></div>
             </div>
         </Content>
     }
