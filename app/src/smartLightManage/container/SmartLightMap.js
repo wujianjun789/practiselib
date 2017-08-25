7cba3be248cb02fc9740c0f89714118698a1f942
吴勇 <wuyong@sansi.com>
2017-08-25 16:04:13 +0800

    fixed #12152

--- app/src/smartLightManage/container/SmartLightMap.js
+++ app/src/smartLightManage/container/SmartLightMap.js
@@ -12,7 +12,7 @@ export default class SmartLightMap extends Component{
         super(props);
         this.state = {
             deviceId:"pole",
-            IsSearch: true,
+            IsSearch: false,
             IsSearchResult: true,
             curDevice:Immutable.fromJS({
               id:1, name:"疏影路灯杆1号", screen:23, charge:45, camera:56, lamp:89, collect:99
@@ -23,8 +23,20 @@ export default class SmartLightMap extends Component{
                 {id:2, name:"疏影路灯杆21号", screen:23,  camera:56, lamp:89, collect:99},
                 {id:3, name:"疏影路灯杆3号", screen:23,  lamp:89, collect:99},
                 {id:4, name:"疏影路灯杆4号", lamp:89, collect:99}
-            ])
+            ]),
+            faultList: [],
+            faultStyle: {"top": "280px"},
+            IsOpenFault: false,
+
+            listStyle:{"maxHeight":"200px"},
+            infoStyle:{"maxHeight":"314"},
+            controlStyle:{"maxHeight":"120"}
         }
+
+        this.screen = Immutable.fromJS({})
+        this.faultKeys = ["sys_fault", "vram_fault", "disp_module_fault", "disp_module_power_fault", "single_pixel_tube_fault",
+            "detection_sys_fault", "ac_fault", "lightning_arrester_fault", "photosensor_fault", "abnormal_temperature_fault",
+            "door_switch_fault"];
 
         this.deviceTypes = [
             {id:"pole", className:"icon_pole"},
@@ -36,7 +48,62 @@ export default class SmartLightMap extends Component{
 
         this.renderInfo = this.renderInfo.bind(this);
         this.renderState = this.renderState.bind(this);
+        this.renderControl = this.renderControl.bind(this);
         this.searchSubmit = this.searchSubmit.bind(this);
+
+        this.setSize = this.setSize.bind(this);
+
+        this.updateCameraVideo = this.updateCameraVideo.bind(this);
+    }
+
+    componentWillMount(){
+        this.mounted = true;
+        window.onresize = event=>{
+            this.mounted && this.setSize();
+        }
+    }
+
+    componentDidMount(){
+        this.mounted && this.setSize();
+    }
+
+    componentWillUnmount(){
+        this.mounted = false;
+        window.onresize = event=>{
+            this.setSize();
+        }
+    }
+
+    setSize(){
+        const {IsSearch} = this.state;
+        let height = window.innerHeight;
+
+        if(IsSearch){
+            let listStyle = {"maxHeight":(height<145?0:height-145)+"px"};
+            this.setState({listStyle:listStyle});
+        }else{
+            let defaultHeight = 0;
+            if(this.refs.poleInfo){
+                defaultHeight = this.refs.poleInfo;
+            }
+            // console.log("domNode:",defaultHeight.style.height);
+            let infoStyle = {"maxHeight":(height<230?0:height-230)};
+            let controlStyle = {"maxHeight":(height<602?0:height-602)};
+            this.setState({infoStyle:infoStyle, controlStyle:controlStyle});
+        }
+    }
+
+    updateCameraVideo(data){
+        if(this.refs.camera != null && data.hasOwnProperty('camera_url')){
+
+            this.client = new JSMpeg.Player(data.camera_url, { canvas: this.refs.camera })
+            /* this.client = new WebSocket(data.camera_url);
+             this.client.onerror = function (err) {
+             console.log(err);
+             }
+             var player = new jsmpeg(this.client, {canvas: this.refs.camera});
+             */
+        }
     }
 
     formatIntl(formatId){
@@ -88,30 +155,111 @@ export default class SmartLightMap extends Component{
     renderInfo(id, props){
         switch(id){
             case "screen":
-                return <div className="state-info screen">
-                    <div className="prop">
+                const {faultList, faultStyle,IsOpenFault} = this.state;
+                return <div className="row state-info screen">
+                    <div className="col-sm-6 prop">
                         {this.renderState(props, "width", "width")}
                         {this.renderState(props, "height", "height")}
                         {this.renderState(props, "version", "version")}
                         {this.renderState(props, "brightness_mode", "brightness_mode", true)}
                         {this.renderState(props, "brightness", "brightness")}
+                        {
+                            <div className="fault-container"><span className="name">{this.formatIntl('app.fault')}:</span><span className={faultList.length>0?"fault":"pass"} onClick={(event)=>{faultList.length>0 && this.faultClick(event)}}></span></div>
+                        }
+                        {
+                            faultList.length>0 &&
+                            <Panel refs="faultPanel" style={faultStyle} className={IsOpenFault?'':'hidden'} title={this.formatIntl('app.fault_info')} closeBtn={true}
+                                   closeClick={this.closeClick}>
+                                {
+                                    faultList.map(key=>{
+                                        return <div key={key}>{this.formatIntl("app."+key)}</div>
+                                    })
+                                }
+                            </Panel>
+                        }
                     </div>
-                    <div className="img-container"><img src=""/></div>
+                    <div className="col-sm-6 img-container"><img src="http://localhost:8080/images/smartLight/screen_test.png"/></div>
                 </div>
+        }
+    }
+    renderControl(id){
+        switch(id){
+            case "screen":
+                return <div className="row state-control screen">
+                        <div className="col-sm-12 form-group switch">
+                            <label className="col-sm-4">显示屏开关:</label>
+                            <select className="col-sm-4">
+                            </select>
+                            <button className="col-sm-3 btn btn-primary padding-left">应用</button>
+                        </div>
+                        <div className="col-sm-12 form-group time-table">
+                            <label className="col-sm-4">时间表1</label>
+                            <select className="col-sm-4">
+                            </select>
+                            <button className="col-sm-3 btn btn-primary">应用</button>
+                        </div>
+                    </div>
+            case "camera":
+                return <div className="row state-control camera">
+                    <div className="col-sm-12 video">
+                        <canvas ref="camera">
+                        </canvas>
+                    </div>
+                    <div className="col-sm-12 form-group focus">
+                        <label className="col-sm-3">变焦:</label>
+                        <div className="col-sm-9">
+                            <input  type="range" min="0" max="100" step="1" value="60" onChange={()=>{}}/>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 form-group preset">
+                        <label className="col-sm-3">预置:</label>
+                        <select className="col-sm-6">
+                        </select>
+                        <button className="col-sm-3 btn btn-primary">切换</button>
+                    </div>
+                </div>
+            case "lamp":
+                return <div className="row state-control lamp">
+                        <div className="col-sm-12 form-group switch">
+                            <label className="col-sm-4">灯开关:</label>
+                            <select className="col-sm-4">
+                            </select>
+                            <button className="col-sm-3 btn btn-primary">应用</button>
+                        </div>
+                        <div className="col-sm-12 form-group strategy">
+                            <label className="col-sm-4">策略调光:</label>
+                            <select className="col-sm-4">
+                            </select>
+                            <button className="col-sm-3 btn btn-primary">应用</button>
+                        </div>
+                        <div className="col-sm-12 form-group group">
+                            <label className="col-sm-4">整组调光:</label>
+                            <label className="col-sm-8 checkbox-inline">
+                                <input type="checkbox"/>
+                                {"疏影路组"}
+                            </label>
+                        </div>
+                        <div className="col-sm-12 form-group handler">
+                            <label className="col-sm-4">手动调光:</label>
+                            <div className="col-sm-8">
+                                <input  type="range" min="0" max="100" step="1" value="60" onChange={()=>{}}/>
+                            </div>
+                        </div>
+                    </div>
         }
     }
 
     render(){
-        const {deviceId, IsSearch, IsSearchResult, curDevice, curId, searchList} = this.state;
+        const {deviceId, IsSearch, IsSearchResult, curDevice, curId, searchList, listStyle, infoStyle, controlStyle} = this.state;
         return (
             <Content>
                 <MapView mapData={{id:"smartLightMap"}}/>
                 <div className="search-container">
                     <div className="searchText">
-                        <input type="search" className="form-control" onChange={()=>{}}/>
+                        <input type="search" className="form-control" placeholder="搜索名称或域" onChange={()=>{}}/>
                         <span className="glyphicon glyphicon-search" onClick={this.searchSubmit}></span>
                     </div>
-                    <ul className={"list-group "+(IsSearchResult?"":"hidden")}>
+                    <ul className={"list-group "+(IsSearch && IsSearchResult?"":"hidden")} style={listStyle}>
                         {
                             searchList.map(pole=>{
                                 return <li key={pole.get("id")} className="list-group-item">
@@ -127,15 +275,15 @@ export default class SmartLightMap extends Component{
                         }
                     </ul>
 
-                    <div className="search-back">
+                    <div className={"margin-top margin-bottom search-back "+(IsSearch?"hidden":"")} style={{"marginBottom":(infoStyle.maxHeight>0?15:0)+"px"}}>
                         <span className="glyphicon glyphicon-menu-left padding-left padding-right"></span>
                         <span className="name">{"返回搜索结果"}</span>
                     </div>
-                    <div className="panel panel-info pole-info">
-                        <div className="panel-heading">
+                    <div ref="poleInfo" id="poleInfo" className={"panel panel-info pole-info "+(IsSearch || infoStyle.maxHeight==0?"hidden":"")} style={Object.assign({"marginBottom":(controlStyle.maxHeight>0?20:0)+"px"},{"maxHeight":infoStyle.maxHeight+"px"})}>
+                        <div className={"panel-heading "+(infoStyle.maxHeight==0?"hidden":"")} style={{"maxHeight":(infoStyle.maxHeight>40?38:infoStyle.maxHeight)+"px"}}>
                             <h3 className="panel-title">{curDevice.get("name")}</h3>
                         </div>
-                        <div className="panel-body">
+                        <div className={"panel-body "+(infoStyle.maxHeight<40?"hidden":"")} style={{"maxHeight":(infoStyle.maxHeight>40?infoStyle.maxHeight-40:0)+"px"}}>
                             <ul className="btn-group">
                                 {curDevice.get("screen") && <li className={" "+"active"}><span className="icon icon_screen_hover"></span></li>}
                                 {curDevice.get("lamp") && <li className={" "+""}><span className="icon icon_lamp"></span></li>}
@@ -148,12 +296,15 @@ export default class SmartLightMap extends Component{
                             }
                         </div>
                     </div>
-                    <div className="panel panel-info pole-control">
-                        <div className="panel-heading">
+                    <div className={"panel panel-info pole-control "+(IsSearch || controlStyle.maxHeight==0?"hidden":"")} style={{"maxHeight":controlStyle.maxHeight+"px"}}>
+                        <div className={"panel-heading "+(controlStyle.maxHeight==0?"hidden":"")} style={{"maxHeight":(controlStyle.maxHeight>40?38:controlStyle.maxHeight)+"px"}}>
                             <h3 className="panel-title">{"设备控制"}</h3>
+                            <span className="glyphicon glyphicon-triangle-bottom"></span>
                         </div>
-                        <div className="panel-body">
-
+                        <div className={"panel-body "+(controlStyle.maxHeight<40?"hidden":"")} style={{"maxHeight":(controlStyle.maxHeight>40?controlStyle.maxHeight-40:0)+"px"}}>
+                            {
+                                this.renderControl(curId)
+                            }
                         </div>
                     </div>
                 </div>
