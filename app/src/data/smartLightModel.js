551314eff21c04dbf58412651d789748884ab44b
Azrael <wuyanbin@sansi.com>
2017-08-31 10:42:01 +0800

    智慧路灯列表添加路由及子页面

--- app/src/data/smartLightModel.js
+++ app/src/data/smartLightModel.js
@@ -1,26 +1,63 @@
 /**
  * Created by a on 2017/8/24.
  */
-export const TreeData=[
+export const TreeData = [
     {
         "id": "map",
-        "name":"地图",
+        "name": "地图",
         "toggled": false,
         "active": true,
         "link": "/smartLight/map",
-        "level":1
+        "level": 1
     },
     {
         "id": "list",
-        "name":"列表",
+        "name": "列表",
         "toggled": false,
         "active": true,
         "link": "/smartLight/list",
-        "level":1
+        "level": 1,
+        "children": [
+            {
+                "id": 'lc',
+                "name": "单灯控制器",
+                "class": "icon_single_lamp_control",
+                "active": false,
+                "link": "/smartLight/list/lc"
+            },
+            {
+                "id": 'lcc',
+                "name": "灯集中控制器",
+                "class": "icon_light_control",
+                "active": false,
+                "link": "/smartLight/list/lcc"
+            },
+            {
+                "id": 'screen',
+                "name": "显示屏",
+                "class": "icon_screen",
+                "active": false,
+                "link": "/smartLight/list/screen"
+            },
+            {
+                "id": 'sensor',
+                "name": "传感器",
+                "class": "icon_sensor_strategy",
+                "active": false,
+                "link": "/smartLight/list/sensor"
+            },
+            {
+                "id": 'collect',
+                "name": "数据采集仪",
+                "class": "icon_collect",
+                "active": false,
+                "link": "/smartLight/list/collect"
+            }
+        ]
     },
     {
         "id": "set",
-        "name":"设置",
+        "name": "设置",
         "toggled": false,
         "active": true,
         "link": "/smartLight/set",
---
+++ app/src/smartLightList/containers/SingleLampCon.js
@@ -0,0 +1,15 @@
+/**
+ * Created by a on 2017/8/14.
+ */
+import React, {Component} from 'react';
+
+export default class SingleLampCon extends Component {
+    constructor(props) {
+        super(props);
+      
+    }
+  
+    render() {
+        return <div>SingleLampCon</div>
+    }
+}
