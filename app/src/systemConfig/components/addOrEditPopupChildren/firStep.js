31a64026fc7c55667a6de81bd816c37c6bbfc3c6
“ChrisWen960216 <957638221@qq.com>
2017-09-04 16:02:31 +0800

    fix a bug.now the whiteList can display as usual

--- app/src/systemConfig/components/addOrEditPopupChildren/firStep.js
+++ app/src/systemConfig/components/addOrEditPopupChildren/firStep.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import PanelFooter from '../../../components/PanelFooter.js';
+import InputCheck from '../../../components/InputCheck.js';
+import Select from '../../../components/Select.1.js';
+import SearchText from '../../../components/SearchText.js';
 
 export default class FirstStepComponet extends Component {
     constructor(props) {
@@ -11,7 +14,15 @@ export default class FirstStepComponet extends Component {
         const props = this.props;
         const footer = <PanelFooter funcNames={ ['onCancel', 'onFlip'] } btnTitles={ ['取消', '下一步'] } {...props}/>;
         return (<div>
-                  <h1>123</h1>
+                  <ul>
+                    <li className='col-sm-6'>路灯名称:
+                      <InputCheck/>
+                    </li>
+                    <li className='col-sm-6'>选择:
+                      <Select/>
+                      <SearchText/>
+                    </li>
+                  </ul>
                   { footer }
                 </div>)
     }
