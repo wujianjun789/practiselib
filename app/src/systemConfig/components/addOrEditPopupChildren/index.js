aca5eea988adcfb3137278007604de020014805f
“ChrisWen960216 <957638221@qq.com>
2017-09-06 10:55:01 +0800

    add CloseFunction into AddOrEditPopup.Try to Detach a Component likes DeveiceList as independentComponent

--- app/src/systemConfig/components/addOrEditPopupChildren/index.js
+++ app/src/systemConfig/components/addOrEditPopupChildren/index.js
@@ -1,13 +1,15 @@
 /** Created By ChrisWen
  *  17/09/04
  *  Declaring: All componets' className all follw the B_E-M(Block_Element-Modify) rules.
- *  Declaring: This componet provides AddOrEdit function as a popup.Import all two childrenCoponents;
+ *  Declaring: This componet provides AddOrEdit function as a popup.Import all two childrenComponents;
  *  ChildrenComponets just recevied all props that from parentComponet, with no logic funtions;
  *  All the datas and logic functions were setted or declared here.
  */
 import React, { Component } from 'react';
-import FirstStepComponet from './firStep.js';
-import SecondStepComponet from './secStep.js';
+import FirstStepComponent from './stepPopup/firStep.js';
+import SecondStepComponent from './stepPopup/secStep.js';
+
+import { overlayerHide } from '../../../common/actions/overlayer.js';
 
 export default class StepComponent extends Component {
     constructor(props) {
@@ -24,13 +26,14 @@ export default class StepComponent extends Component {
         })
     }
 
-    onCancel() {}
+
 
     render() {
-
+        const props = this.props;
+        console.log('index.props', props);
         const StepComponent = this.state.showFirstPage === true
-            ? <FirstStepComponet onFlip={ this.onFlip } disabled/>
-            : <SecondStepComponet onFlip={ this.onFlip } />
+            ? <FirstStepComponent {...props} onFlip={ this.onFlip } disabled/>
+            : <SecondStepComponent {...props} onFlip={ this.onFlip } />
         return (<div>
                   { StepComponent }
                 </div>)
