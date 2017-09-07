8357938f290f432659df7578fa87a1bfd4454748
“ChrisWen960216 <957638221@qq.com>
2017-09-07 10:07:16 +0800

    add EditPopup-Select funtions(Immutable-dataSource).Detaching a model provide initFunctions/data

--- app/src/systemConfig/components/EditPopupComponent.js
+++ app/src/systemConfig/components/EditPopupComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PanelFooter from '../../components/PanelFooter.js';
-import Select from '../../components/Select.1.js';
+import Select from '../../components/Select.js';
 import SearchText from '../../components/SearchText.js';
 import DeviceList from './list/index.js';
 
@@ -14,27 +14,17 @@ export default class EditPopupComponet extends Component {
         const props = this.props;
         console.log('EditPopup.props', props)
         const footer = <PanelFooter funcNames={ [null, 'onConfirmed'] } btnTitles={ [null, '完成'] } {...props}/>;
-        const data = [{
-            name: '摄像头1',
-            added: true
-        }, {
-            name: '摄像头2',
-            added: true
-        }, {
-            name: '摄像头3',
-            added: false
-        }];
         return (<div id='edit-popup' className='clearfix'>
                   <span>选择设备:</span>
                   <div className='edit_selectdevice-content clearfix'>
                     <div>
-                      <Select className='edit_selectdevice-select' />
+                      <Select className='edit_selectdevice-select' onChange={ this.props.onChange } data={ this.props.equipmentSelectList } />
                       <SearchText className='edit_selectdevice-searchtext' placeholder='输入设备名称' />
-                      <DeviceList className='edit_selectdevice-ul' data={ data } operations={ ['添加', '已添加'] } />
+                      <DeviceList className='edit_selectdevice-ul' operations={ ['添加', '已添加'] } {...props}/>
                     </div>
                     <div className='edit_whole-content clearfix'>
                       <ul className='edit_whole-ul'>
-                        <DeviceList className='edit_selectdevice-ul' data={ data } showIcon={ true } {...props}/>
+                        <DeviceList className='edit_selectdevice-ul' showIcon={ true } {...props}/>
                       </ul>
                     </div>
                   </div>
