335dd86406f02dd78681176441bd25de8ff7c2f0
“ChrisWen960216 <957638221@qq.com>
2017-09-18 08:51:14 +0800

    Optimizing EditPopupComponet code.Add some explains for nessary

--- app/src/systemConfig/components/EditPopupComponent.js
+++ app/src/systemConfig/components/EditPopupComponent.js
@@ -4,28 +4,24 @@ import Select from '../../components/Select.1.js';
 import SearchText from '../../components/SearchText.js';
 import DeviceList from './list/index.js';
 
-
 export default class EditPopupComponet extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const props = this.props;
-    const {equipmentSelectList, search} = props;
+    const {equipmentSelectList, allPoleEquipmentsData, allEquipmentsData, search, onChange, itemClick, deleteClick, searchTextOnChange, value, searchAssets} = props;
     const footer = <PanelFooter funcNames={ [null, 'onConfirmed'] } btnTitles={ [null, '完成'] } {...props}/>;
     return (<div id='edit-popup' className='clearfix'>
               <span>选择设备:</span>
               <div className='edit_selectdevice-content clearfix'>
                 <div>
-                  <Select className='edit_selectdevice-select' onChange={ this.props.onChange } {...equipmentSelectList}/>
-                  <SearchText className='edit_selectdevice-searchtext' placeholder={ search.placeholder } onChange={ this.props.searchTextOnChange } value={ this.props.value } submit={ this.props.searchAssets }
+                  <Select className='edit_selectdevice-select' onChange={ onChange } {...equipmentSelectList}/>
+                  { /* We use {...props} to replace itemClick={itemClick}.*/ }
+                  <SearchText className='edit_selectdevice-searchtext' placeholder={ search.placeholder } onChange={ searchTextOnChange } value={ value } submit={ searchAssets }
                   />
-                  <DeviceList className='edit_selectdevice-ul' operations={ ['添加', '已添加'] } data={ this.props.allEquipmentsData } itemClick={ this.props.itemClick } />
+                  <DeviceList className='edit_selectdevice-ul' operations={ ['添加', '已添加'] } data={ allEquipmentsData } {...props}/>
                 </div>
                 <div className='edit_whole-content clearfix'>
                   <ul className='edit_whole-ul'>
-                    <DeviceList className='edit_selectdevice-ul' showIcon={ true } data={ this.props.allPoleEquipmentsData } iconClassName='delete' itemClick={ this.props.deleteClick } />
+                    <DeviceList className='edit_selectdevice-ul' showIcon={ true } iconClassName='delete' data={ allPoleEquipmentsData } itemClick={ deleteClick } />
                   </ul>
                 </div>
               </div>
