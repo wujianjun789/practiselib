c19f20551d598d53279ae9d4d8c95a9ff82d5780
吴勇 <wuyong@sansi.com>
2017-09-21 14:37:29 +0800

    fixed 12880

--- app/src/app/action/index.js
+++ app/src/app/action/index.js
@@ -2,6 +2,7 @@
  * Created by a on 2017/8/10.
  */
 import {getHttpHeader, getModuleConfig} from '../../util/network'
+import {getCookie} from '../../util/cache'
 import {
     MODULE_INIT
 } from '../actionType/index'
@@ -9,7 +10,8 @@ export function getModule() {
     let headers = getHttpHeader();
 
     return dispatch=>{
-       getModuleConfig(response=>{
+        let user = getCookie("user");
+       getModuleConfig(user, response=>{
             dispatch({type:MODULE_INIT, data:response})
        }, err=>{
            throw (err);
