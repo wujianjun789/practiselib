4b9f8aebc295304ce767990f56286ae12ddcd421
xuyijun <xuyijun@sansi.com>
2017-10-09 13:19:44 +0800

    update modulePopup

--- app/src/permissionManage/container/ModulePopup.js
+++ app/src/permissionManage/container/ModulePopup.js
@@ -1,18 +1,22 @@
-import React,{PureComponent} from 'react';
+import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import Panel from '../../components/Panel';
 import PanelFooter from '../../components/PanelFooter';
 import '../../../public/styles/permissionManage.less';
-import {IsExitInArray} from '../../util/algorithm';
+import {IsExitInArray,getObjectByKeyObj,spliceInArray} from '../../util/algorithm';
 
-export default class ModulePopup extends PureComponent{
+export default class ModulePopup extends Component{
     constructor(props){
         super(props);
-        const {data,isEdit=false} = this.props;
+        const {data=[]} = this.props;
+        this.state = {
+            moduleList:data
+        }
         this.onCancel = this.onCancel.bind(this);
         this.onConfirm = this.onConfirm.bind(this);
-        this.getCheckedModule = this.getCheckedModule.bind(this);
+        this.moduleAdd = this.moduleAdd.bind(this);
+        this.moduleDelete =this.moduleDelete.bind(this);
     }
 
     onCancel(){
@@ -21,38 +25,60 @@ export default class ModulePopup extends PureComponent{
 
     onConfirm(){
         let data = {
-            modules:this.getCheckedModule(),
+            modules:this.state.moduleList,
             id:this.props.id
         }
         this.props.onConfirm(data,true);
         this.props.overlayerHide();
     }
 
-    getCheckedModule(){
-        let modules = document.getElementsByName('module');
-        let checked_val = [];
-        for(let i in modules){
-            if(modules[i].checked)
-                checked_val.push(modules[i].value);
-        }
-        return checked_val;
+    moduleDelete(item){
+        let {moduleList} = this.state;
+        spliceInArray(moduleList,item);
+        this.setState({moduleList:moduleList});
+    }
+
+    moduleAdd(item){
+        let {moduleList} = this.state;
+        moduleList.push(item);
+        this.setState({moduleList:moduleList});        
     }
 
     render() {
         let {className = '',title = '',modules,id,data=[]} = this.props;
+        let {moduleList} = this.state;
         let footer = <PanelFooter funcNames={['onCancel','onConfirm']} btnTitles={['取消','确认']} btnClassName={['btn-default', 'btn-primary']} btnDisabled={[false, false]} onCancel={this.onCancel} onConfirm={this.onConfirm}/>;
         return (
             <Panel className={className} title = {title} footer = {footer} closeBtn = {true} closeClick = {this.onCancel}>
                 <div className = 'form-group row module-per'>
                     <label className="control-label">模块权限:</label>
-                    <div>
-                        {
-                            modules.map(item=>{
-                                return <label className="checkbox-inline" key={item.key}>
-                                    <input type="checkbox" name='module' value={item.key} defaultChecked={IsExitInArray(data,item.key)}/> {item.title}
-                                </label>
-                            })
-                        }
+                    <div className="module-content">
+                        <div className='all-module-list'>
+                            <ul>
+                            {
+                                modules.map((item,index)=>{
+                                    return <li key = {index}>
+                                        {    
+                                            IsExitInArray(moduleList,item.key)?<span className='list-node-add'>已添加</span>:<span className='glyphicon glyphicon-plus' onClick={()=>this.moduleAdd(item.key)}></span>
+                                        }
+                                        {item.title}                                        
+                                    </li>
+                                })
+                            }
+                            </ul>
+                        </div>
+                        <div className='module-list'>
+                            <ul>
+                                {
+                                    moduleList.map((item,index)=>{
+                                        return <li key = {index}>
+                                            <span className="icon-table-delete" onClick={()=>this.moduleDelete(item)}></span>
+                                            {getObjectByKeyObj(modules,'key',item)['title']}
+                                        </li>
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </Panel>
