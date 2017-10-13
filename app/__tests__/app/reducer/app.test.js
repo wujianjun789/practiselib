117cc8fccb312377cd176638d43c0f21b1d123b7
Azrael <wuyanbin@sansi.com>
2017-10-13 16:43:57 +0800

    更新app/reducer/index.js，补充测试

--- app/__tests__/app/reducer/app.test.js
+++ app/__tests__/app/reducer/app.test.js
@@ -1,5 +1,19 @@
-import app, {initialState as state} from '../../../src/app/reducer/index';
+import app, {initialState as state, moduleInfo, moduleInit} from '../../../src/app/reducer/index';
+import * as actionTypes from '../../../src/app/actionType';
 
 test('test app reducer', () => {
-    expect(app(undefined, {})).toEqual(state);
+	expect(app(undefined, {})).toEqual(state);
+
+	const data = Object.keys(moduleInfo).map(key => ({key}) );
+	const expectItems = [];
+	Object.keys(moduleInfo).forEach(key =>{
+		expectItems.push(moduleInfo[key]);
+	});
+	const action = {type: actionTypes.MODULE_INIT, data: data};
+	expect(app(undefined, action )).toEqual({...state, items: expectItems });
 });
+
+test('test moduleInit function', () => {
+	const data1 = [{key: 'default'}];
+	expect(moduleInit({}, data1)).toEqual({items: [{key: '', title: '', link: ''}]});
+})
