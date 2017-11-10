d0b2f1ac9db30aad737dcc2347d47fe149c9f9cd
黄涛(研发一部） <uid12403@sansi.com>
2017-11-10 11:36:09 +0800

    update

--- app/src/mediaPublish/component/materialVideo.js
+++ app/src/mediaPublish/component/materialVideo.js
@@ -4,33 +4,39 @@ import React, { Component } from 'react'
 export default class Video extends Component {
     constructor(props) {
         super(props);
+        this.video=undefined;
         this.state = {
             name: '',
             path: '',
-            url:null
+            url: null
         }
-        this.changeName = this.changeName.bind(this);
-        this.importMaterial = this.importMaterial.bind(this);
+
     }
 
-    changeName(e) {
+    changeName=(e)=>{
         this.setState({ name: e.target.value })
     }
 
-    importMaterial(e) {
-        const self=this;
-        const file=e.target.files[0];
-        const url=URL.createObjectURL(file);
-        this.setState({path:file.name,url:url})
+    importMaterial=(e)=> {
+        const file = e.target.files[0];
+        const url = URL.createObjectURL(file);
+        if(url){
+            this.video.controls='controls'
+        }
+        this.setState({ path: file.name, url: url })
+    }
+    refsCb=(node)=>{
+        this.props.focus(node);
+        this.video=node;
     }
     render() {
-        const { name, path,url } = this.state;
+        const { name, path, url } = this.state;
         return (
             <div className='material'>
                 <div>
                     <span>素材名称</span>
                     <input type='text' value={name} onChange={this.changeName} />
-                    <span className={name?"m-prompt m-hidden":"m-prompt"}>请输入名称</span>
+                    <span className={name ? "m-prompt m-hidden" : "m-prompt"}>请输入名称</span>
                 </div>
                 <div className='import'>
                     <span>导入素材</span>
@@ -38,11 +44,11 @@ export default class Video extends Component {
                         {path ? path : '选择列表文件路径'}
                         <label htmlFor='select-file' className='glyphicon glyphicon-link'></label>
                         <input type="file" accept="video/*" onChange={this.importMaterial} />
-                        <span className={path?"m-prompt m-hidden":"m-prompt"}>请选择文件</span>
+                        <span className={path ? "m-prompt m-hidden" : "m-prompt"}>请选择文件</span>
                     </div>
                 </div>
                 <div className='show'>
-                    <video src={url} controls autoPlay loop/>
+                    <video src={url} ref={this.refsCb} autoPlay loop />
                 </div>
             </div>
         )
