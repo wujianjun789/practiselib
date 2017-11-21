b266ff10903070affab55d9d83fb699680ce560b
“ChrisWen960216 <957638221@qq.com>
2017-11-21 11:15:31 +0800

    Fill data into select

--- app/src/mediaPublish/component/digitalClock.js
+++ app/src/mediaPublish/component/digitalClock.js
@@ -4,21 +4,53 @@
  */
 
 import React, { Component } from 'react';
-import PanelFooter from '../../components/PanelFooter.js'
+import PanelFooter from '../../components/PanelFooter.js';
+import _digitalClock from '../config/digitalClock.js';
 import '../../../public/styles/digitalClock.less';
 
  export default class DigitalClock extends Component {
    constructor(props) {
      super(props);
      this.state = {
-       config:{
-         key: 'digitalClock',
-         title: '属性'
-       }
+       config: _digitalClock
      }
+   }
+   componentDidMount() {
+     const { config } = this.state;
    }
    render() {
      const { config } = this.state;
+     const timeZone  = config.timeZone.map((item,index) => {
+       for(let k in item){
+         return <option value={item[k]} key={index}>{k}</option>
+       }
+     });
+     const fontFamily = config.fontFamily.map((item,index) => {
+       for(let k in item){
+         return <option value={item[k]} key={index}>{k}</option>
+       }
+     });
+     const fontSize = config.fontSize.map((item,index) => {
+       for(let k in item){
+         return <option value={item[k]} key={index}>{k}</option>
+       }
+     });
+     const location = config.location.map((item,index) => {
+       for(let k in item){
+         return <option value={item[k]} key={index}>{k}</option>
+       }
+     });
+     const timeFormat = config.timeFormat.map((item,index) => {
+       for(let k in item){
+         return <option value={item[k]} key={index}>{k}</option>
+       }
+     });
+     const dateFormat = config.dateFormat.map((item,index) => {
+       for(let k in item){
+         return <option value={item[k]} key={index}>{k}</option>
+       }
+     });
+
      return(
        <div className='pro-container digitalClock' id='digitalClock'>
         <ul>
@@ -31,7 +63,9 @@ import '../../../public/styles/digitalClock.less';
           <li>
             <div>
               <div>时区</div>
-              <div><select/></div>
+              <div>
+                <select>{timeZone}</select>
+              </div>
             </div>
             <div>
               <div>播放时长</div>
@@ -51,11 +85,15 @@ import '../../../public/styles/digitalClock.less';
           <li>
             <div>
               <div>选择字体</div>
-              <div><select/></div>
+              <div>
+                <select>{fontFamily}</select>
+              </div>
             </div>
             <div>
               <div>文字大小</div>
-              <div><select/></div>
+              <div>
+                <select>{fontSize}</select>
+              </div>
             </div>
             <div>
               <div>文字颜色</div>
@@ -65,17 +103,23 @@ import '../../../public/styles/digitalClock.less';
           <li>
             <div>
               <div>区域设置</div>
-              <div><select/></div>
+              <div>
+                <select>{location}</select>
+              </div>
             </div>
           </li>
           <li>
             <div>
               <div>日期格式</div>
-              <div><select/></div>
+              <div>
+                <select>{dateFormat}</select>
+              </div>
             </div>
             <div>
               <div>时间格式</div>
-              <div><input type='text'/></div>
+              <div>
+                <select>{timeFormat}</select>
+              </div>
             </div>
             <div>
               <div>单行显示</div>
