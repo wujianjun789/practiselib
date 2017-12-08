27abb23ce3dde79747a3ef4f0ef376ce9e01146c
吴勇 <wuyong@sansi.com>
2017-12-08 15:13:25 +0800

    fixed #15835

--- app/src/mediaPublish/component/PlayerPlan.js
+++ app/src/mediaPublish/component/PlayerPlan.js
@@ -202,7 +202,7 @@ class PlayerPlan extends PureComponent{
             </div>
             <div className="row">
                 <button className="btn btn-primary pull-right" onClick={() => this.planClick('apply')}><FormattedMessage id='mediaPublish.apply'/></button>
-                <button className="btn btn-gray pull-right" onClick={()=>this.planClick('reset')}><FormattedMessage id='mediaPublish.reset'/></button>
+                <button className="btn btn-gray margin-right-1 pull-right" onClick={()=>this.planClick('reset')}><FormattedMessage id='mediaPublish.reset'/></button>
             </div>
         </div>
     }
