701cb69cc701c83c8665a897e2b894fa1a110eab
黄涛(研发一部） <uid12403@sansi.com>
2018-01-09 10:13:28 +0800

    update report route

---
+++ app/src/reporterManage/count/containers/Energy.js
@@ -0,0 +1,11 @@
+
+
+import React, { PureComponent } from 'react'
+
+export default class Energy extends PureComponent {
+	render() {
+		return (
+			<div>this is energy</div>
+		)
+	}
+}
---
+++ app/src/reporterManage/device/containers/Lc.js
@@ -0,0 +1,11 @@
+
+
+import React, { PureComponent } from 'react'
+
+export default class Lc extends PureComponent {
+	render() {
+		return (
+			<div>this is lc</div>
+		)
+	}
+}
