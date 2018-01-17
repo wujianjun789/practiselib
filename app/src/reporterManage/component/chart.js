90bfea0895c0d77d5bef1fdb37f3a108b9fa15fb
黄涛(研发一部） <uid12403@sansi.com>
2018-01-17 16:32:46 +0800

    add d3 in reporterManage

--- app/src/reporterManage/component/chart.js
+++ app/src/reporterManage/component/chart.js
@@ -6,30 +6,49 @@ export default class Chart extends PureComponent {
     static propTypes = {
         start: PropTypes.object,
         end: PropTypes.object,
-        data: PropTypes.object
+        data: PropTypes.array
     }
     componentWillMount() {
+        this._isMounted = true;
         this.chart = null;
     }
-
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.data !== this.props.data) {
+            this.updateLineChart(nextProps);
+        }
+    }
     //d3图表
     drawLineChart = (node) => {
-        const { data, start, end } = this.props;
+        const { start, end } = this.props;
         this.chart = new DrawChart({
             wrapper: node,
-            data: Object.values(data),
+            data: [],
             xAccessor: d => d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.timestamp),
             yAccessor: d => d.value,
             xDomain: [start, end],
             yDomain: [0, 100],
             curveFactory: d3.curveStepAfter,
-            yTickFormat: d => { if (d === 0) return ''; return `${d}` },
+            yTickFormat: d => { if (d === 0) return ''; return `${d}%` },
             tooltipAccessor: d => d.y
         })
     }
-    getChartData = (cb) => {
+
+    updateLineChart = (props) => {
+        const { data, start, end } = props;
+        this.chart.updateChart(data, [start, end])
     }
 
+    destroyLineChart = () => {
+        this.chart.destroy();
+        this.chart = null;
+    }
+
+    componentWillUnmount() {
+        this.destroyLineChart();
+        this._isMounted = false;
+    }
+
+    //ref的回调函数 只在挂载或卸载的时候调用
     render() {
         return (
             <div class='chart-container' ref={this.drawLineChart}></div>
