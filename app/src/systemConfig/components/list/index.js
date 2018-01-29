3d652498556bdc9f0834ebca2973338f8b9901bd
ChrisWen960216 <chriswen96@163.com>
2018-01-29 08:22:51 +0800

    Fix a bug that all div must have a role if they has been added for onClick or another events

--- app/src/systemConfig/components/list/index.js
+++ app/src/systemConfig/components/list/index.js
@@ -25,7 +25,7 @@ export default class DeviceList extends Component {
       /*item.add will control the firstdiv's status.
       You can use this property to control the operations(array)'s value such as index below */
       let index = item.added === true ? 1 : 0;
-      return showIcon === true ? <span className="icon_delete delete"></span> : operations[index];
+      return showIcon === true ? <span className={iconClassName}></span> : operations[index];
     };
 
     const deviceList = data.length === 0 ? null : data.map((item, index) => {
@@ -33,7 +33,7 @@ export default class DeviceList extends Component {
         <div>
           { item.name }
         </div>
-        <div onClick={ (e) => this.onClick(item) } role="button">
+        <div onClick={ () => this.onClick(item) } role="presentation">
           { showDiv(item) }
         </div>
       </li>);
