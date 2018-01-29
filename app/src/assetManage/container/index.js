89eddbabda64871b94350aad42e9962bfac3e5b4
马迅 <maxun@sansi.com>
2018-01-29 10:19:32 +0800

    esint fix in assetManage

--- app/src/assetManage/container/index.js
+++ app/src/assetManage/container/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by a on 2017/7/17.
  */
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
@@ -19,72 +19,72 @@ import {treeViewInit} from '../../common/actions/treeView';
 
 import {sideBarToggled} from '../action/index';
 import {treeViewNavigator} from '../../common/util/index';
-class AssetManageIndex extends Component{
-    constructor(props){
-        super(props);
+class AssetManageIndex extends Component {
+  constructor(props) {
+    super(props);
 
-        this.initTreeData = this.initTreeData.bind(this);
-        this.onToggle = this.onToggle.bind(this);
+    this.initTreeData = this.initTreeData.bind(this);
+    this.onToggle = this.onToggle.bind(this);
+  }
+
+  componentWillMount() {
+    this.mounted = true;
+    getModelData(() => {this.mounted && this.initTreeData();});
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  componentDidMount() {
+  }
+
+  initTreeData() {
+    this.props.actions.treeViewInit(TreeData);
+    treeViewNavigator(TreeData, this.props.router);
+  }
+
+  onToggle(node) {
+    const {actions} = this.props;
+    actions.sideBarToggled(node);
+  }
+
+  render() {
+    let parentPath = '';
+    let childPath = '';
+    const {routes} = this.props;
+    if (routes.length > 3) {
+      parentPath = routes[3].path;
     }
 
-    componentWillMount(){
-        this.mounted = true;
-        getModelData(()=>{this.mounted && this.initTreeData();});
+    if (routes.length > 4) {
+      childPath = routes[4].path;
     }
 
-    componentWillUnmount(){
-        this.mounted = false;
-    }
-
-    componentDidMount(){
-    }
-
-    initTreeData(){
-        this.props.actions.treeViewInit(TreeData);
-        treeViewNavigator(TreeData, this.props.router);
-    }
-
-    onToggle(node){
-        const {actions} = this.props;
-        actions.sideBarToggled(node);
-    }
-
-    render(){
-        let parentPath = '';
-        let childPath = '';
-        const {routes} = this.props;
-        if(routes.length>3){
-            parentPath = routes[3].path;
-        }
-
-        if(routes.length>4){
-            childPath = routes[4].path;
-        }
-
-        return <div className={'container '+'asset-'+parentPath+' '+parentPath+'-'+childPath}>
-            <HeadBar moduleName="app.asset.manage" router={this.props.router}/>
-            <SideBar onToggle={this.onToggle}/>
-            {this.props.children}
-            <Overlayer />
-        </div>;
-    }
+    return <div className={'container ' + 'asset-' + parentPath + ' ' + parentPath + '-' + childPath}>
+      <HeadBar moduleName="app.asset.manage" router={this.props.router}/>
+      <SideBar onToggle={this.onToggle}/>
+      {this.props.children}
+      <Overlayer />
+    </div>;
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        userCenter:state.userCenter
-    };
+  return {
+    userCenter:state.userCenter,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators({
-            treeViewInit: treeViewInit,
-            sideBarToggled: sideBarToggled
-        }, dispatch)
-    };
+  return {
+    actions: bindActionCreators({
+      treeViewInit: treeViewInit,
+      sideBarToggled: sideBarToggled,
+    }, dispatch),
+  };
 }
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(injectIntl(AssetManageIndex));
