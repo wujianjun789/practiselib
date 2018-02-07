61be0c8275b2cd8b14f3d467d968ccbaf7162867
ChrisWen960216 <chriswen96@163.com>
2018-02-07 09:49:56 +0800

    Add playItem.id check functions

--- app/src/mediaPublish/component/previewImgList.js
+++ app/src/mediaPublish/component/previewImgList.js
@@ -1,88 +1,48 @@
-import React from 'react';
+/** Created By ChrisWen
+ * 检查预览播放项数组的各ID是否真实有效。
+ * /
 
-export default class PreviewImgList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      previewPlayList: [],
-    };
-  }
 
-  componentDidMount() {
-    console.log('DIDMOUNT');
-  }
+const previewList = [{ areaId: 1, playItemId: 10 }, { areaId: 2, playItemId: 2 }, { areaId: 3, playItemId: 3 }];
+const sourcePlayItemList = [{areaId:1, playItemList:[1, 2, 3, 4, 5]}, {areaId:2, playItemList:[3, 2]}, {areaId:3, playItemList:[2, 3]}];
 
-  componentDidUpdate() {
-    this.setPlayItemArray();
-  }
+/*
+ * @param {Array<previewItem>} previewList 
+ * @param {object} sourcePlayItemList 
+ */
 
-  setPlayItemArray() {
-    let arealist = [];
-    let targetType = '';
-    const props = this.props.props;
-    if (props.parentNode) {
-      targetType = props.parentNode.type;
-    }
+function getPreviewListCheck(previewList, sourcePlayItemList) {
+  // 检查区域是否一致，如果区域少了，删除多余的区域。
+  const areaList = sourcePlayItemList.map(item => { return item.areaId; });
+  const areaCheckedPreviewList = getAreaCheck(previewList, areaList);
+  // 检查播放项是否一致，如果播放项不存在，改为65535
+  const itemsCheckedPrevielist = getPlayItemsCheck(areaCheckedPreviewList, sourcePlayItemList);
+  return itemsCheckedPrevielist;
   
-    if (targetType === 'scene') {
-      arealist = props.parentNode.children; 
-    }
-    console.log('激活setPlayItemArray', targetType, arealist);
-    if (arealist === [] || targetType === '') {
-      return undefined;
-    } else {
-      const itemList = arealist.map(item => {
-        if (item.id === props.curNode.id) {
-          return {areaId:item.id, playItemId:props.playerListAsset.get('id')};
-        } else {
-          const playlist = this.state.previewPlayList;
-          if (playlist === []) {
-            return { areaId: item.id, playItemId: 65535 };
-          } else {
-            for (let i = 0; i < playlist.length; i++) {
-              if (playlist[i].areaId === item.id) {
-                return playlist[i];
-              } else {
-                return { areaId: item.id, playItemId:item.id || 65535 };
-              }
-            }
-          }
-          return { areaId: item.id, playItemId: 65535 };
-        }
-      });
-      // console.log('ITEMLIST', itemList);
-      // if (this.state.previewPlayList !== itemList) {
-      //   this.setState({
-      //     previewPlayList: itemList,
-      //   });
-      // }  
-    }
-  }
-
-  getPreviewImg() {
-    const projectId = this.props.project.id;
-    const programId = this.props.parentParentNode.id;
-    const sceneId = this.props.parentNode.id;
-    const zoneId = this.props.curNode.id;
-    const items = this.props.previewPlayList.map(item => { return item.playItemId; });
-    const requestJson = ({ projectId, programId, sceneId, zoneId, items });
-    return console.log(requestJson);
-    // return previewPlayItem(requestJson, data => { this.setprops({ previewSrc:data }); });
-  }
-
-  render() {
-    return null;
-  }
 }
 
-// function curry(func, fixedParams) {
-//   if ( !Array.isArray(fixedParams) ) { fixedParams = []; }
-//   return function() {
-//     let newParams = Array.prototype.slice.call(arguments); // 新传的所有参数
-//     if ( (fixedParams.length + newParams.length) < func.length ) {
-//       return curry(func, fixedParams.concat(newParams));
-//     } else {
-//       return func.apply(undefined, fixedParams.concat(newParams));
-//     }
-//   };
-// }
+/**
+ * 检查区域是否一一匹配
+ * @param {object} previewList 
+ * @param {Array<Number>} sourceAreaList 
+ */
+
+function getAreaCheck(previewList, sourceAreaList) {
+  const areaCheckedPreviewList = previewList.filter(item => { return sourceAreaList.indexOf(item.areaId) > -1; });
+  return areaCheckedPreviewList;
+}
+
+function getPlayItemsCheck(areaCheckedPreviewList, sourcePlayItemList) {
+  const itemsCheckedPrevielist = areaCheckedPreviewList.map((item) => {
+    const { areaId = -1 } = item;
+    for (let i = 0; i < sourcePlayItemList.length; i++) {
+      if (sourcePlayItemList[i].areaId === areaId && sourcePlayItemList[i].playItemList.indexOf(item.playItemId) > -1) {
+        return item;
+      }
+    }
+    item.playItemId = 65535;
+    return item;
+  });
+  return itemsCheckedPrevielist;
+}
+console.log(getPreviewListCheck(previewList, sourcePlayItemList));
