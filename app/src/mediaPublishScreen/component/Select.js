1054aa0885ef092c349cb498c35dda5df5548de6
黄涛 <huangtao@sansi.com>
2018-02-09 11:01:09 +0800

    处理弹框隐藏后默认跳转至初始option

---
+++ app/src/mediaPublishScreen/component/Select.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Select = (props, context) => {
+    const { id, className = '', disabled = false, hidden = false, options, onChange, current } = props;
+
+    let defaultValue = options[0].name;
+    if (current) {
+        defaultValue = current.name;
+    }
+
+    return (
+        <select className={`form-control select ${className}`} disabled={disabled} id={id} onChange={onChange}
+            value={defaultValue}>
+            {options.map((item, index) => {
+                return <option key={index} hidden={item.hidden} value={item.name}>{item.name}</option>;
+            })}
+        </select>
+    );
+};
+
+Select.propTypes = {
+    id: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    hidden: PropTypes.bool,
+    options: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onChange: PropTypes.func.isRequired,
+    current: PropTypes.any,
+};
+
+export default Select;
