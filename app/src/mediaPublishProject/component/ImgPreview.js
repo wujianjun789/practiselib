107a9764e63a46097423f4bfa091243518f3e736
ChrisWen960216 <chriswen96@163.com>
2018-03-13 14:17:14 +0800

    Add imgPreview component

--- app/src/mediaPublishProject/component/ImgPreview.js
+++ app/src/mediaPublishProject/component/ImgPreview.js
@@ -1,22 +1,45 @@
 /** Created By ChrisWen
- *  ImgPreview Component
  *  18/03/13
+ *  PreviewImgComponent
+ */
+import React from 'react';
+import '../../../public/styles/imgPreview.less';
+
+/** props
+ *  1. wapperSize --- the size of whole component 
+ *  1. projectSize --- Container Size
+ *  2. arealist --- [{areaId,areaStyle,playItemUrl}]
+ *  3. areaStyle: {width, height, top(offsetY), left(offsetX)}
+ *  4. selectedId --- selectedArea highLight
  */
 
-import React, { Component } from 'react';
+const initSize = { width: 1000, height: 1000 };
 
-export default class ImgPreview extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-  }
+function renderAreaList(areaList, selectedId) {
+  const renderList = areaList.map((area) => {
+    const {id, style} = area;
 
-  render() {
-    return (<div>Hello React</div>);
-  }
+    const className = id === selectedId ? 'active' : '';
+    return (
+      <div className={`areaEle ${className}`} key={id} style={style}>
+        <img src={area.src} alt="" />
+      </div>);
+  });
+  return renderList;
 }
 
-function mapStateToProps() { }
-function mapDispatchToProps() {}
+const PreviewContainer = (props) => {
+  /* We have reached an agreement that 65535 is the number can not be touched.
+   * 65535 means null;
+   */
+  const { wapperSize = initSize, projectSize = initSize, areaList, selectedId = 65535 } = props;
+  const areaListEle = renderAreaList(areaList, selectedId);
+  return (
+    <div className="container-wapper" style={wapperSize}>
+      <div id="preview-container" style={projectSize}>{areaListEle}</div>
+    </div>
+  );
+};
+
+
+export default PreviewContainer;
