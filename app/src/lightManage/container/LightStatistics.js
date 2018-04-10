7dc08f939dfcbff966dad9977385d08dc8075ab2
马迅 <maxun@sansi.com>
2018-04-10 16:04:01 +0800

    照明统计功能更新以及国际化

--- app/src/lightManage/container/LightStatistics.js
+++ app/src/lightManage/container/LightStatistics.js
@@ -3,7 +3,7 @@
  */
 import '../../../public/styles/lightManage-statistics.less';
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import Content from '../../components/Content';
 import Select from '../../components/Select.1';
 import BarChart from '../utils/barChart';
@@ -11,12 +11,13 @@ import PieChart from '../utils/pieChart';
 
 import { getDomainList } from '../../api/domain';
 import { getEnergy, getDomainStatusByDomainId } from '../../api/statistics';
-// import { injectIntl, FormattedMessage } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
 
-export default class LightStatistics extends Component {
+export class LightStatistics extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      energyOption: 'month',
       energyFilt: {
         id: '',
         title: '',
@@ -93,13 +94,13 @@ export default class LightStatistics extends Component {
       // planState: {id:'1', domainId:'1', planTotal:'25', palnSuccess:'23', planFail:'22'},
       // lightState: {id:'1', domainId:'1', lightTotal:'180', lightOnPercent:'95',lighton:'195', lightOff:'10'},
       domainStatus:
-                {
-                  // id: '',
-                  // domainId: '1',
-                  // deviceTotal: '100', deviceOnlineRunning: '90', deviceOnlineError: '3', deviceOffline: '7',
-                  // planTotal: '25', palnSuccess: '23', planFail: '2',
-                  // lightTotal: '180', lightOnPercent: '0.95', lighton: '195', lightOff: '10',
-                },
+        {
+          // id: '',
+          // domainId: '1',
+          // deviceTotal: '100', deviceOnlineRunning: '90', deviceOnlineError: '3', deviceOffline: '7',
+          // planTotal: '25', palnSuccess: '23', planFail: '2',
+          // lightTotal: '180', lightOnPercent: '0.95', lighton: '195', lightOff: '10',
+        },
       pieWIdth: 150,
       pieHeight: 170,
     };
@@ -115,6 +116,7 @@ export default class LightStatistics extends Component {
     this.energySelect = this.energySelect.bind(this);
     this.getEnergyDataByIdAndType = this.getEnergyDataByIdAndType.bind(this);
     this.getStatusDataByDomainId = this.getStatusDataByDomainId.bind(this);
+    this.formatIntl = this.formatIntl.bind(this);
   }
 
   componentWillMount() {
@@ -138,7 +140,13 @@ export default class LightStatistics extends Component {
     //     }, 1500);
     //   }
     // };
-    
+
+  }
+
+  formatIntl(formatId) {
+    const {intl} = this.props;
+    return intl ? intl.formatMessage({id:formatId}) : null;
+    // return formatId;
   }
 
   initData() {
@@ -179,23 +187,23 @@ export default class LightStatistics extends Component {
     let day = ('0' + (myDate.getDate())).slice(-2);
     var dateTime, str1a, str1b;
     switch (type) {
-    case '1'://获取一年中每个月的能耗
-      str1a = `${year}-01-01T00:00:00Z`;
-      str1b = `${year}-12-31T23:59:59Z`;
-      dateTime = { between: [str1a, str1b] };
-      break;
-    case '2'://获取一个月中每天的能耗
-      str1a = `${year}-${month}-01T00:00:00Z`;
-      str1b = `${year}-${month}-31T23:59:59Z`;
-      dateTime = { 'between': [str1a, str1b] };
-      break;
-    case '3'://获取一天中每小时的能耗
-      str1a = `${year}-${month}-${day}T00:00:00Z`;
-      str1b = `${year}-${month}-${day}T23:59:59Z`;
-      dateTime = { between: [str1a, str1b] };
-      break;
-    default:
-      return dateTime;
+      case '1'://获取一年中每个月的能耗
+        str1a = `${year}-01-01T00:00:00Z`;
+        str1b = `${year}-12-31T23:59:59Z`;
+        dateTime = { between: [str1a, str1b] };
+        break;
+      case '2'://获取一个月中每天的能耗
+        str1a = `${year}-${month}-01T00:00:00Z`;
+        str1b = `${year}-${month}-31T23:59:59Z`;
+        dateTime = { 'between': [str1a, str1b] };
+        break;
+      case '3'://获取一天中每小时的能耗
+        str1a = `${year}-${month}-${day}T00:00:00Z`;
+        str1b = `${year}-${month}-${day}T23:59:59Z`;
+        dateTime = { between: [str1a, str1b] };
+        break;
+      default:
+        return dateTime;
     }
     // console.log("dateTime:", dateTime);
     //获取能耗数据并重新渲染
@@ -203,16 +211,20 @@ export default class LightStatistics extends Component {
       let { data } = this.state.energyFilt;
       let { totalEnergy } = this.state;
       totalEnergy = 0;
-      if (response.length!==0) { data = []; 
-      for (var key in response) {
-        totalEnergy += response[key].value;
-        let dataItem = {};
-        dataItem.x = response[key].dateTime;
-        dataItem.y = response[key].value;
-        data.push(dataItem);
-      }};
-      this.setState({ energyFilt: Object.assign({}, this.state.energyFilt, { data }),
-        totalEnergy: totalEnergy }, () => {
+      if (response.length !== 0) {
+        data = [];
+        for (var key in response) {
+          totalEnergy += response[key].value;
+          let dataItem = {};
+          dataItem.x = response[key].dateTime;
+          dataItem.y = response[key].value;
+          data.push(dataItem);
+        }
+      };
+      this.setState({
+        energyFilt: Object.assign({}, this.state.energyFilt, { data }),
+        totalEnergy: totalEnergy
+      }, () => {
       });
     });
   }
@@ -224,41 +236,41 @@ export default class LightStatistics extends Component {
       domainStatus = [];
       for (var i = 0; i < response.length; i++) {
         switch (response[i].variable) {
-        case 'deviceTotal':
-          domainStatus.deviceTotal = response[i].value;
-          break;
-        case 'deviceOnlineRunning':
-          domainStatus.deviceOnlineRunning = response[i].value;
-          break;
-        case 'deviceOnlineError':
-          domainStatus.deviceOnlineError = response[i].value;
-          break;
-        case 'deviceOffline':
-          domainStatus.deviceOffline = response[i].value;
-          break;
-        case 'planTotal':
-          domainStatus.planTotal = response[i].value;
-          break;
-        case 'palnSuccess':
-          domainStatus.palnSuccess = response[i].value;
-          break;
-        case 'planFail':
-          domainStatus.planFail = response[i].value;
-          break;
-        case 'lightTotal':
-          domainStatus.lightTotal = response[i].value;
-          break;
-        case 'lightOnPercent':
-          domainStatus.lightOnPercent = response[i].value;
-          break;
-        case 'lighton':
-          domainStatus.lighton = response[i].value;
-          break;
-        case 'lightOff':
-          domainStatus.lightOff = response[i].value;
-          break;
-        default:
-          return 1;
+          case 'deviceTotal':
+            domainStatus.deviceTotal = response[i].value;
+            break;
+          case 'deviceOnlineRunning':
+            domainStatus.deviceOnlineRunning = response[i].value;
+            break;
+          case 'deviceOnlineError':
+            domainStatus.deviceOnlineError = response[i].value;
+            break;
+          case 'deviceOffline':
+            domainStatus.deviceOffline = response[i].value;
+            break;
+          case 'planTotal':
+            domainStatus.planTotal = response[i].value;
+            break;
+          case 'palnSuccess':
+            domainStatus.palnSuccess = response[i].value;
+            break;
+          case 'planFail':
+            domainStatus.planFail = response[i].value;
+            break;
+          case 'lightTotal':
+            domainStatus.lightTotal = response[i].value;
+            break;
+          case 'lightOnPercent':
+            domainStatus.lightOnPercent = response[i].value;
+            break;
+          case 'lighton':
+            domainStatus.lighton = response[i].value;
+            break;
+          case 'lightOff':
+            domainStatus.lightOff = response[i].value;
+            break;
+          default:
+            return 1;
         }
 
       }
@@ -285,8 +297,21 @@ export default class LightStatistics extends Component {
   }
 
   energySelect(event) {
+    this.setState({ energyOption: event.target.id });
+    let id = event.target.id;
     let { energyList, type } = this.state;
-    let index = event.target.selectedIndex;
+    let index = 1;
+    switch (id) {
+      case 'year':
+        index = '0';
+        break;
+      case 'month':
+        index = '1';
+        break;
+      case 'day':
+        index = '2';
+        break;
+    }
     type = energyList.options[index].type;
     energyList.index = index;
     energyList.value = energyList.options[index].name;
@@ -326,19 +351,19 @@ export default class LightStatistics extends Component {
       } else if (refId == 'deviceState') {
         // data = [5, 6, 3, 2];
         data = [domainStatus.deviceOnlineRunning ? domainStatus.deviceOnlineRunning : 0,
-          domainStatus.deviceOnlineError ? domainStatus.deviceOnlineError : 0,
-          domainStatus.deviceOffline ? domainStatus.deviceOffline : 0,
+        domainStatus.deviceOnlineError ? domainStatus.deviceOnlineError : 0,
+        domainStatus.deviceOffline ? domainStatus.deviceOffline : 0,
         ];
         this.drawChart(ref, data);
       } else if (refId == 'planState') {
         // data = [8, 4];
         data = [domainStatus.palnSuccess ? domainStatus.palnSuccess : 0,
-          domainStatus.planFail ? domainStatus.planFail : 0];
+        domainStatus.planFail ? domainStatus.planFail : 0];
         this.drawChart(ref, data);
       } else if (refId == 'lightState') {
         // data = [9, 2];
         data = [domainStatus.lightOnPercent ? (domainStatus.lightOnPercent) * 100 : 0,
-          (1 - domainStatus.lightOnPercent) ? (1 - domainStatus.lightOnPercent) * 100 : 0,
+        (1 - domainStatus.lightOnPercent) ? (1 - domainStatus.lightOnPercent) * 100 : 0,
         ];
         // console.log("data:", data);
         this.drawChart(ref, data);
@@ -353,7 +378,7 @@ export default class LightStatistics extends Component {
     if (ref.id == 'energyStatistics') {
       this.barchart = new BarChart({
         wrapper: ref,
-        width:ref.parentNode.offsetWidth,
+        width: ref.parentNode.offsetWidth,
         // width:ref.clientWith,
         // wrapper: ref.parentNode,
         data: data,
@@ -367,7 +392,7 @@ export default class LightStatistics extends Component {
         wrapper: ref,
         data: data,
         width: boxLeftwidth,
-        height:'170',        
+        height: '170',
       });
       this.piechart && this.state.piechartList.push(this.piechart);
     }
@@ -380,19 +405,24 @@ export default class LightStatistics extends Component {
 
   }
   render() {
-    const { domainList, energyList, domainStatus } = this.state;
+    const { domainList, energyList, domainStatus, energyOption } = this.state;
     return (
       <Content>
         <div className="heading">
           <Select className="sort" id="domain" valueField={domainList.valueField} titleField={domainList.titleField}
             value={domainList.value} options={domainList.options} onChange={this.domainSelect} />
-          <button className="btn btn-primary" onClick={this.onClick}>应用</button>
+          {/* <button className="btn btn-primary" onClick={this.onClick}>应用</button> */}
         </div>
 
         <div className="energy-container panel panel-default">
-          <div className="energy-container-header panel-heading">能耗图
-          <Select className="sort" id="energy" valueField={energyList.valueField} titleField={energyList.titleField}
-            value={energyList.value} options={energyList.options} onChange={this.energySelect} />
+          <div className="energy-container-header panel-heading">{this.formatIntl('lightManage.Statistics.energy')}
+          <div className="timeSelect">
+              <button className={`timeSelectItem ${energyOption == 'day' ? 'selected' : ''}`} id="day" onClick={this.energySelect}>{this.formatIntl('lightManage.Statistics.day')}</button>
+              <button className={`timeSelectItem ${energyOption == 'month' ? 'selected' : ''}`} id="month" onClick={this.energySelect}>{this.formatIntl('lightManage.Statistics.month')}</button>
+              <button className={`timeSelectItem ${energyOption == 'year' ? 'selected' : ''}`} id="year" onClick={this.energySelect}>{this.formatIntl('lightManage.Statistics.year')}</button>
+            </div>
+            {/* <Select className="sort" id="energy" valueField={energyList.valueField} titleField={energyList.titleField}
+              value={energyList.value} options={energyList.options} onChange={this.energySelect} /> */}
           </div>
           <div className="row panel-body lightenergychart">
             <div className="col-sm-9 col-xs-9">
@@ -400,8 +430,8 @@ export default class LightStatistics extends Component {
             </div>
             <div className="col-sm-3 col-xs-3">
               <div className="energyInfo">
-                <div className="fontbold font">域： <span>{`${this.state.domainList.value}`}</span></div>
-                <div className="fontbold font">设备总能耗：<span>{`${this.state.totalEnergy}KWh`}</span></div>
+                <div className="fontbold font">{this.formatIntl('lightManage.Statistics.domain')}： <span>{`${this.state.domainList.value}`}</span></div>
+                <div className="fontbold font">{this.formatIntl('lightManage.Statistics.totalEnergy')}：<span>{`${this.state.totalEnergy}KWh`}</span></div>
               </div>
             </div>
           </div>
@@ -410,7 +440,7 @@ export default class LightStatistics extends Component {
           <div className="row" id="pieBox" >
             <div className="col-sm-4 col-xs-4">
               <div className=" panel">
-                <div className="panel-heading">设备状态图
+                <div className="panel-heading">{this.formatIntl('lightManage.Statistics.deviceState')}
                 </div>
                 <div className="panel-body statecircle1 ">
                   <div className="boxLeft">
@@ -418,14 +448,14 @@ export default class LightStatistics extends Component {
                   </div>
                   <div className="boxRight">
                     <div className="stateInfo">
-                      <div className="fontbold font">域： <span>{`${this.state.domainList.value}`}</span></div>
-                      <div className="fontbold font">设备数：<span>
+                      <div className="fontbold font">{this.formatIntl('lightManage.Statistics.domain')}： <span>{`${this.state.domainList.value}`}</span></div>
+                      <div className="fontbold font">{this.formatIntl('lightManage.Statistics.deviceCount')}：<span>
                         {`${domainStatus.deviceTotal ? domainStatus.deviceTotal : ''}`}</span></div>
-                      <div className="dot color1"></div><div className="font">在线正常数：<span>
+                      <div className="dot color1"></div><div className="font">{this.formatIntl('lightManage.Statistics.deviceOnlineRunning')}：<span>
                         {`${domainStatus.deviceOnlineRunning ? domainStatus.deviceOnlineRunning : ''}`}</span></div>
-                      <div className="dot color2"></div><div className="font">在线故障数：<span>
+                      <div className="dot color2"></div><div className="font">{this.formatIntl('lightManage.Statistics.deviceOnlineError')}：<span>
                         {`${domainStatus.deviceOnlineError ? domainStatus.deviceOnlineError : ''}`}</span></div>
-                      <div className="dot color3"></div><div className="font">离线数：<span>
+                      <div className="dot color3"></div><div className="font">{this.formatIntl('lightManage.Statistics.deviceOffline')}：<span>
                         {`${domainStatus.deviceOffline ? domainStatus.deviceOffline : ''}`}</span></div>
                     </div>
                   </div>
@@ -434,7 +464,7 @@ export default class LightStatistics extends Component {
             </div>
             <div className="col-sm-4 col-xs-4">
               <div className=" panel">
-                <div className="panel-heading">计划执行状态
+                <div className="panel-heading">{this.formatIntl('lightManage.Statistics.planState')}
                 </div>
                 <div className="panel-body statecircle2">
                   <div className="boxLeft">
@@ -442,12 +472,12 @@ export default class LightStatistics extends Component {
                   </div>
                   <div className="boxRight">
                     <div className="stateInfo">
-                      <div className="fontbold font">域： <span>{`${this.state.domainList.value}`}</span></div>
-                      <div className="fontbold font">计划数：<span>
+                      <div className="fontbold font">{this.formatIntl('lightManage.Statistics.domain')}： <span>{`${this.state.domainList.value}`}</span></div>
+                      <div className="fontbold font">{this.formatIntl('lightManage.Statistics.planCount')}：<span>
                         {`${domainStatus.planTotal ? domainStatus.planTotal : ''}`}</span></div>
-                      <div className="dot color1"></div><div className="font">成功计划数：<span>
+                      <div className="dot color1"></div><div className="font">{this.formatIntl('lightManage.Statistics.palnSuccess')}：<span>
                         {`${domainStatus.palnSuccess ? domainStatus.palnSuccess : ''}`}</span></div>
-                      <div className="dot color2"></div><div className="font">失败计划数：<span>
+                      <div className="dot color2"></div><div className="font">{this.formatIntl('lightManage.Statistics.planFail')}：<span>
                         {`${domainStatus.planFail ? domainStatus.planFail : ''}`}</span></div>
                     </div>
                   </div>
@@ -456,15 +486,15 @@ export default class LightStatistics extends Component {
             </div>
             <div className="col-sm-4 col-xs-4">
               <div className=" panel">
-                <div className="panel-heading">亮灯率</div>
+                <div className="panel-heading">{this.formatIntl('lightManage.Statistics.lightOn')}</div>
                 <div className="panel-body statecircle3">
                   <div className="boxLeft">
                     <div id="lightState" className="circle" ref={ref => { this.renderChart(ref); }}></div>
                   </div>
                   <div className="boxRight">
                     <div className="stateInfo">
-                      <div className="fontbold font">域：<span>{`${this.state.domainList.value}`}</span></div>
-                      <div className="fontbold font">亮灯率：<span>{domainStatus.lightOnPercent ? 
+                      <div className="fontbold font">{this.formatIntl('lightManage.Statistics.domain')}：<span>{`${this.state.domainList.value}`}</span></div>
+                      <div className="fontbold font">{this.formatIntl('lightManage.Statistics.lightOn')}：<span>{domainStatus.lightOnPercent ?
                         domainStatus.lightOnPercent * 100 + '%' : ''}</span></div>
                     </div>
                   </div>
@@ -477,3 +507,18 @@ export default class LightStatistics extends Component {
     );
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+  };
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(injectIntl(LightStatistics));
