603183ead767e482f4ec63e0ab1298c64be3b09e
ChrisWen960216 <chriswen96@163.com>
2018-04-11 15:19:08 +0800

    Using local font file

--- webpack.config.js
+++ webpack.config.js
@@ -2,79 +2,87 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: path.resolve(__dirname, 'app/src/root/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'app', 'public'),
-        filename: '[name].bundle.js',
-        publicPath: '/',
-        chunkFilename: '[name].[chunkhash].chunk.js',
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'app/public'),
-        historyApiFallback: true,
-        inline: true,
-        hot: true,
-        host: '0.0.0.0',
-        port: 18080
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                include: __dirname,
+const iconUrl = '/font/font_zck90zmlh7hf47vi';
+const theme = {'@icon-url':`"${iconUrl}"`};
 
+module.exports = {
+  entry: path.resolve(__dirname, 'app/src/root/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'app', 'public'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'app/public'),
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    host: '0.0.0.0',
+    port: 18080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: __dirname,
+
+      },
+      {
+        test: /\.css|\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true,
             },
-            {
-                test: /\.css|\.less$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true,
-                        }
-                    },
-                    'postcss-loader',
-                    'less-loader'
-                ]
+          },
+          'postcss-loader',
+          {
+            loader:'less-loader',
+            options:{
+              modifyVars:theme,
             },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: {
-                            minimize: true,
-                        }
-                    }
-                ],
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
             },
-            {
-                test:/\.(png|jpe?g|gif|)$/,
-                loader:'url-loader'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DllReferencePlugin({
-            context: __dirname,
-            manifest: require('./manifest.json'),
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('development')
-            }
-        }),
-        new HtmlwebpackPlugin({
-            filename: 'index.html',
-            template: 'app/src/templates/index.html',
-            inject: true,
-            hash: true
-        }),
+          },
+        ],
+      },
+      {
+        test:/\.(png|jpe?g|gif|)$/,
+        loader:'url-loader',
+      },
     ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require('./manifest.json'),
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
+    new HtmlwebpackPlugin({
+      filename: 'index.html',
+      template: 'app/src/templates/index.html',
+      inject: true,
+      hash: true,
+    }),
+  ],
 };
